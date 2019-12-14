68b2b8528279a60e594ba7f99cf6121acc9e75f1
marcoamolinaro <molinaromarcoaurelio@gmail.com>
2019-12-14 16:18:09 -0300

    41. State in Action

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 
 const CounterScreen = () => {
+
+    const [counter, setCounter] = useState(0);    
+
     return (
         <View>
-            <Text>Current Count:</Text>
+            <Button title="Increse" onPress={()=>{
+                setCounter(counter + 1);
+            }} />
+            <Button title="Decrese" onPress={()=>{
+                setCounter(counter - 1);
+            }}/>
+            <Text>Current Count: {counter}</Text>
         </View>
     );
 };
